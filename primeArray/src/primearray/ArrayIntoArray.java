60e1ab703862d17a306b7ac8c9c399ebc412d65f
Damjan Stojcev <109067762+damjanstojcev4@users.noreply.github.com>
2022-11-04 17:35:32 +0100

    Update ArrayIntoArray.java

--- primeArray/src/primearray/ArrayIntoArray.java
+++ primeArray/src/primearray/ArrayIntoArray.java
@@ -11,4 +11,27 @@ public class ArrayIntoArray {
 		tempArray[tempArray.length - 1] = x;
 		return tempArray;
 	}
+		public int[] addElementInOrder(int[] arr, int x) {
+		int[] tempArray = new int[arr.length + 1];
+		int index = 0;
+		
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] >= x) {
+				index = i;
+				break;
+			}
+		}
+		
+		for (int i = 0; i < index; i++) {
+			tempArray[i] = arr[i];
+		}
+		
+		tempArray[index] = x;
+		
+		for (int i = index; i < arr.length; i++) {
+			tempArray[i + 1] = arr[i];
+		}
+		
+		return tempArray;
+	}
 }
